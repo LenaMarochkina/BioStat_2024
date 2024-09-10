90d4981b4dd7b754213895f87cd6aaf9839bfa33
lenamarochkina <marochkina.lena@gmail.com>
2024-09-10 15:33:48 +0200

    feat: select, slice, filter, mutate, group, replace, delete, rename, sort, pivot, separate, unite functions

--- GitHub_task/Work_with_dataframes.R
+++ GitHub_task/Work_with_dataframes.R
@@ -79,9 +79,200 @@ data %>%
   ungroup() %>%
   select(`Гемоглобин_E1`, `Гемоглобин_E2`, `Average_hemoglobin`)
   
+# Select columns
+data %>%
+  select(`Группа.крови`, `Рост`)
 
+data %>% 
+  select(!`Рост` & !`Пол`)
 
+data %>%
+  select(where(is.numeric))
 
+data %>%
+  select(where(is.factor))
 
+data %>% 
+  select(`Группа`, (function(x) is.factor(x)) | where(function(x) is.numeric(x)))
+
+# Select data by visit 
+data %>%
+  select(contains("_E1"))
+
+data %>%
+  select(matches("_E\\d{1}"))
+
+data %>%
+  select(starts_with("Б"))
+
+# Select strictly
+var <- c("Базофилы_E1", "Базофилы_E2", "Гемоглобин_E1")
+
+data %>% 
+  select(all_of(var))
+
+# Select gentle
+
+data %>%
+  select(any_of(var))
+
+# Conditional select - AND, OR, NEG
+data %>%
+  select(where(is.numeric)) %>%
+  select(where(function(x) sd(x, na.rm = TRUE) > 2 & mean(x, na.rm = TRUE) < 10))
+
+data %>%
+  select(where(is.numeric)) %>%
+  select(where(function(x) sd(x, na.rm = TRUE) > 2 | mean(x, na.rm = TRUE) < 10 | median(x, na.rm = TRUE) > 5))
+
+data %>%
+  select(!where(is.numeric))
+
+# Select other columns 
+data %>% 
+  select(`Пол`, `Эритроциты_E1`, everything())
+
+# Select and change names
+data %>%
+  select("Erythrocytes_V1" = `Эритроциты_E1`, "Erythrocytes_V2" = `Эритроциты_E2`)
+
+# Select rows
+data %>%
+  slice(1:10)
+
+data %>%
+  slice_head(n = 10)
+
+data %>%
+  slice_tail(prop = 0.2)
+
+data %>%
+  slice_sample(prop = 0.1)
+
+data %>%
+  slice_min(`Возраст`)
+
+# Data filtration
+data %>%
+  filter(`Пол` == "Женский" & `Группа.крови` %in% c("A (II)", "B (III)") & `Группа` != "Группа 1" )
+
+data %>%
+  filter(between(`Возраст`, 31, 34))
+
+data %>%
+  filter(near(`Эозинофилы_E1`, 3.38, tol = 0.1))
+
+data %>%
+  filter(if_all(.cols = contains("Базофилы"), .fns = function(x) x > 1.5))
+
+data %>%
+  filter(if_any(.cols = contains("Базофилы"), .fns = function(x) x > 1.5))
+  
+data %>%
+  group_by(`Группа`) %>%
+  filter(`Возраст` > 30)
+
+# Data mutation
+data %>%
+  mutate(`Women_4th_blood_type` = ifelse(`Пол` == "Женский" & `Группа.крови` == "AB (IV)", "Y", "N")) %>%
+  select(`Women_4th_blood_type`, everything()) %>%
+  arrange(desc(`Women_4th_blood_type`))
+
+# Arithmetic operations
+tibble(var_1 = 1:10, var_2 = var_1 + 1.123) %>%
+  mutate (var_sum = var_1 + var_2,
+          var_minus = var_1 - var_2,
+          var_multiple = var_1 * var_2,
+          var_divide = var_1 / var_2,
+          var_1_log = log(var_1),
+          var_1_log1p = log1p(var_1),
+          var_1_exp = exp(var_1_log),
+          var_1_exm1 = expm1 (var_1_log1p),
+          var_2_round = round (var_2, 2),
+          var_2_ceil = ceiling (var_2),
+          var_2_floor = floor (var_2)) %>%
+  glimpse()
+
+# Use a case-when option
+data %>%
+  mutate(`Age_group` = case_when(`Возраст` < 20 ~ "<20",
+                                 between(`Возраст`, 20, 30) ~ "20-30",
+                                 `Возраст` > 30 ~ ">30") %>% as.factor()) %>%
+  select(`Возраст`, `Age_group`) %>%
+  arrange(`Возраст`)
+
+# Replace NA
+data %>%
+  mutate(`Группа.крови` =`Группа.крови` %>% as.character() %>% replace_na("No data") %>% as.factor())
+
+data %>%
+  mutate(`Группа.крови` =`Группа.крови` %>% na_if("AB (IV)"))
+
+# Delete variable through mutate()
+data %>% 
+  mutate(`Группа.крови` = NULL)
+
+# Across() function
+data %>%
+  mutate(across(where(is.numeric), function(x) (x - mean(x, na.rm = TRUE)) / sd(x, na.rm = TRUE)))
+
+# Iteration through rows
+data %>%
+  rowwise() %>%
+  mutate(`Basophils_Mean` = mean(c_across(contains("Базофилы")))) %>%
+  ungroup() %>%
+  select(`Basophils_Mean`, contains("Базофилы"))
+
+data %>%
+  group_by(`Группа`) %>%
+  mutate(across(contains("Базофилы"), function(x) x - mean(x, na.rm = TRUE))) %>%
+  ungroup() %>%
+  select(`Группа`, contains("Базофилы"))
+
+# Rename variables
+data %>%
+  rename(`Group` = `Группа`) %>%
+  select(`Group`)
+
+data %>%
+  rename_with(function(x) x %>% stri_replace_all_regex(c("_E1", "_E2"), c("_V1", "_V2"), vectorize_all =  FALSE)) %>%
+  glimpse()
+
+new_name <- "Group"
+data %>%
+  rename(!! new_name := `Группа`) %>%
+  glimpse()
+
+# Sorting
+data %>%
+  arrange(`Пол`, desc(`Возраст`))
+
+data %>%
+  group_by(`Группа`) %>%
+  arrange(`Возраст`, .by_group = TRUE)
+
+# Pivoting data frames 
+data %>%
+  select(`Группа`, contains("_E1")) %>%
+  pivot_longer(!`Группа`)
+
+data %>%
+  select(`Группа`, contains("_E1")) %>%
+  mutate(ID = row_number()) %>%
+  pivot_longer(!c(`Группа`, ID)) %>%
+  pivot_wider(id_cols = ID)
+
+# Unique values
+data %>%
+  distinct(`Группа`, .keep_all = TRUE)
+
+# Separate variables
+tibble(var_1 = rep(paste0("first part", "__", "second_part"), 10)) %>% 
+  separate(var_1, into = c("var_1", "var_2"), sep = "__")
+
+# Unite variables
+tibble(var_1 = rep(paste0("first part", "__", "second_part"), 10)) %>% 
+  separate(var_1, into = c("var_1", "var_2"), sep = "__") %>%
+  unite("new_var", var_1, var_2, sep = " AND ")
 
 
